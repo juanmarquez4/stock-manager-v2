a2c189cf435e2f2cec1dfddbb4ed4c4d68f66def
juanmarquez4 <67566810+juanmarquez4@users.noreply.github.com>
2023-05-25 16:07:37 +1000

    Updated components/Product to include products list

--- client/src/components/Product.js
+++ client/src/components/Product.js
@@ -1,64 +1,47 @@
-import React, {useState} from "react";
-import { useNavigate } from 'react-router-dom';
-import { useQuery, useMutation } from "@apollo/client";
-import { QUERY_PRODUCT } from "../utils/queries";
-import { CREATE_PRODUCT } from "../utils/mutations";
+import React from "react";
+import {useQuery} from "@apollo/client";
+import {QUERY_PRODUCT} from "../utils/queries";
+
 
 
 function Product() {
     const {data} = useQuery(QUERY_PRODUCT);
-
     const productList = data?.product || [];
-
-    const [formData, setFormData] = useState({
-        product: 'Beer',
-    });
-    let navigate = useNavigate();
-
-    const [createProduct, {error}] = useMutation(CREATE_PRODUCT);
-
-    const handleInputChange = (event) => {
-        const { name, value } = event.target;
-        setFormData({ ...formData, [name]: value });
-      };
     
-      const handleFormSubmit = async (event) => {
-        event.preventDefault();
-    
-        try {
-          const { data } = await createProduct({
-            variables: { ...formData },
-          });
-    
-          navigate(`/product/${data.createProduct._id}`);
-        } catch (err) {
-          console.error(err);
-        }
-    
-        setFormData({
-          product: 'Beer',
-        });
-      };
-
     return (
-        <div className="card bg-white card-rounded w-50">
-            <form onSubmit={handleFormSubmit}>
-                <label>Select New Product</label>
-                <select name="product" onChange={handleInputChange}>
-                    {productList.map((product) => {
-                        return (
-                            <option key={product._id} value={product.name}>
-                                {product.name}
-                            </option>
-                        );
-                    })}
-                </select>
-                <button className="btn btn-danger" type="submit">
-                  Create Product!
-                </button>
-            </form>
-            {error && <div>Something went wrong...</div>}
-        </div>
+      <>
+      <div className="card-body m-5">
+        <h2>List of products:</h2>
+        <ul>
+          {productList.map((product) => {
+            return (
+              <li key={product._id}>{product.name}</li>
+            )
+          })}
+        </ul>
+      </div>
+      <div className="card-footer text-center m-3">
+        <h2>Ready to create a new product?</h2>        
+      </div>
+      <div className="card bg-white card-rounded w-50">
+        <form>
+          <label>Select New Product</label>
+          <select>
+              {productList.map((product) => {
+                  return (
+                      <option key={product._id} value={product.name}>
+                          {product.name}
+                      </option>
+                  );
+              })}
+          </select>
+          <button className="btn btn-danger" type="submit">
+            Create Product!
+          </button>
+        </form>
+      </div>
+      </>
+      
     )
 };
 
